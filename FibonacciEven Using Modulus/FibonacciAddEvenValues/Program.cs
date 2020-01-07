0692723c4856d4abe378a310cdf369f3f1f57bbb
Ediezzle <edmore.mandikiyana@gmail.com>
2020-01-07 15:16:49 +0200

    final commit

---
+++ FibonacciEven Using Modulus/FibonacciAddEvenValues/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace FibonacciAddEvenValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your desired maximum limit which shouldn't be exceeded by the largest element in the Fibonacci sequence. N.B: More like ceiling value, not the number of elements!");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int max = Int32.Parse(input);
+                Console.WriteLine("Your chosen Fibonacci sequence is as follows: ");
+                Fibonacci(max);
+            }
+          
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse '{input}'. Please restart the program and enter a valid integer number");
+            }
+            
+
+        }
+
+        //max is the largest value an element in the fibonacci sequence can take
+        static void Fibonacci(int max)
+        {
+            //Recursive formula for the Fibonacci Sequence Numbers
+            /*
+            *Recurrence for the Fibonacci sequence is:
+                Fn = Fn-1 + Fn-2
+            with seed values
+                F0 = 0, F1 = 1.
+            Where Fn means n'th term in the sequence    
+            */
+            int firstFibElement = 0;
+            int secondFibElement = 1;
+
+            //evenValuesTotal which will store the sum of even elements in the series is initialised to 0
+            int evenValuesTotal = 0;
+
+            //The while loop iterates through the sequence up until it reaches max (largest number than all the elements in the sequence)
+            while (firstFibElement <= max)
+            {
+                //print elements of the sequence to the console
+                Console.Write(firstFibElement + " ");
+
+                //checking if an element in the sequence is even, and if so, then incrementing the variable evenValuesTotal (sum of the even elements before it in the sequence)
+                if (firstFibElement % 2 == 0 && evenValuesTotal+firstFibElement < 5000000)
+                {
+                    evenValuesTotal += firstFibElement;
+
+                }
+
+                int sum = firstFibElement + secondFibElement;
+                firstFibElement = secondFibElement;
+                secondFibElement = sum;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Sum of even-valued terms in your Fibonacci sequence which doesn't exceed 5 million is: " + evenValuesTotal);
+        }
+    }
+}
---
+++ FibonacciEven Using Recursion/EvenFibonacciSum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace EvenFibonacciSum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hey anonymous user, please enter your desired maximum limit which shouldn't be exceeded by the sum of all the even numbers in the Fibonacci sequence. N.B: More like ceiling value, not the number of elements!: ");
+            string input = Console.ReadLine();
+            try
+            {
+                int limit = Int32.Parse(input);
+                Console.WriteLine($"The sum of even numbers in yor chosen Fibonnaci sequence, which doesn't exceed  5 million is: {evenFibonacciSum(limit)}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse '{input}'. Please restart the program and enter a valid integer number");
+            }
+        }
+
+        static int evenFibonacciSum(int limit)
+        {
+
+            //Recursive formula for even Fibonacci Numbers
+            /*
+            *Recurrence for Even Fibonacci sequence is:
+                EFn = 4EFn-1 + EFn-2
+            with seed values
+                EF0 = 0, EF1 = 2.
+            Where EFn means n'th term in Even Fibonacci sequence    
+            */
+
+            if (limit < 2)
+            {
+                return 0;
+            }
+
+            //Initialize first two even
+            //numbers and their sum
+            int firstEvenFib = 0;
+            int secondEvenFib = 2;
+
+            int sum = firstEvenFib + secondEvenFib;
+
+            //calculating sum of even
+            //Fibonacci value
+            //with a condition that sum, after being incremented, cannot be greater than 5 000 000
+            while (secondEvenFib <= limit && (sum + secondEvenFib) < 5000000)
+            {
+                // get next even value of Fibonacci sequence 
+                int thirdEvenFib = 4 * secondEvenFib + firstEvenFib;
+
+                // Move to next even number 
+                // and update sum 
+                firstEvenFib = secondEvenFib;
+                secondEvenFib = thirdEvenFib;
+                sum += secondEvenFib;
+            }
+            return sum;
+        }
+    }
+}
